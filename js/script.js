324c905197415f179134a74eb23e27b13b7bc08f
hamisicodes <newhamisi@gmail.com>
2020-03-04 15:11:36 +0300

    save changes to my branch

--- js/script.js
+++ js/script.js
@@ -1,25 +1,61 @@
 //constructor
-function Dog(breed,age){
+function Dog(breed,age,mode){
     this.breed = breed;
     this.age = age ;
+    this.mode = mode;
 }
 
-//Function that calculates price of the Dog
+//Function that calculates price of the Dog based on the age and breed
 
 Dog.prototype.calculatePrice = function(){
-    if(this.breed === 'Bull Dog'){
-        this.price = 50000;
-    }else if(this.breed === 'Belgian Malinois'){
-        this.price = 60000;
-    }else if(this.breed === 'Rotweiler'){
-        this.price = 70000 ;
-    }else if(this.breed === 'German Shepherd'){
-        this.price = 80000 ;
-    }else{
-        this.price = 0 ;
+
+    switch(this.age){
+        case 'Adult':
+            if(this.breed === 'Bull Dog'){
+                this.price = 50000;
+            }else if(this.breed === 'Belgian Malinois'){
+                this.price = 60000;
+            }else if(this.breed === 'Rotweiler'){
+                this.price = 70000 ;
+            }else if(this.breed === 'German Shepherd'){
+                this.price = 80000 ;
+            }
+            break;
+
+        case 'Puppy':
+            case 'Adult':
+            if(this.breed === 'Bull Dog'){
+                this.price = 25000;
+            }else if(this.breed === 'Belgian Malinois'){
+                this.price = 30000;
+            }else if(this.breed === 'Rotweiler'){
+                this.price = 35000 ;
+            }else if(this.breed === 'German Shepherd'){
+                this.price = 40000 ;
+            }
+            break;
+            
+
     }
-    return this.price ;
+
+    return this.age ;
+    
+   
 } 
+
+//Function that calculates the delivery fee
+
+Dog.prototype.calculateDelivery = function(){
+    if(this.mode === 'Delivery'){
+        this.deliveryFee =  1000 ;
+    }else if(this.mode === 'No Delivery'){
+        this.deliveryFee = 0 ;
+    }
+    return this.deliveryFee;
+}
+
+
+
 
 
 $(document).ready(function(){
@@ -45,20 +81,77 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 
-    $('#guard').click(function(){     // click event for guard dogs
+    $('#guard').click(function(event){     // click event for guard dogs
         var dogBreed =  $('#breed-1 option:selected').val();
         var dogAge   =  $('#age-1 option:selected').val();
+        var delivered  =  $('#delivery-1 option:selected').val();
+        var quantity = parseInt($('#quantity').val());
 
-        var newDog = new Dog(dogBreed,dogAge);
-        dogPrice = newDog.calculatePrice();
-        alert('The Breed of the Dog is ' + newDog.breed + ' the Age is ' + newDog.age + ' and the price is ' + newDog.price);
+        var newDog = new Dog(dogBreed,dogAge,delivered);
+         dogPrice = newDog.calculatePrice();
+         dogDeliveryFee = newDog.calculateDelivery();
 
+
+       
+
+        alert('The Breed of the Dog is ' + newDog.breed + ' the Age is ' + newDog.age + ' Number of dogs ordered is ' + quantity + ' and the price is ' + (quantity * newDog.price)  + ' mode of purchase is '
+         + newDog.mode + ' and the Delivery fee is '  + newDog.deliveryFee) ;
+        event.preventDefault();
 
     })
+    
 
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //navigation bar
