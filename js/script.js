48b8b8129fbfe3edeeb9b03813d83450c16e37a5
njerittah <vivianbianca92@gmail.com>
2020-03-05 13:21:03 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,82 @@
 //constructor
-function Dog(breed,age){
+function Dog(breed, age, mode) {
     this.breed = breed;
-    this.age = age ;
+    this.age = age;
+    this.mode = mode;
 }
 
 //Function that calculates price of the Dog
 
-Dog.prototype.calculatePrice = function(){
-    if(this.breed === 'Bull Dog'){
-        this.price = 50000;
-    }else if(this.breed === 'Belgian Malinois'){
-        this.price = 60000;
-    }else if(this.breed === 'Rotweiler'){
-        this.price = 70000 ;
-    }else if(this.breed === 'German Shepherd'){
-        this.price == 80000 ;
-    }else{
-        this.price = 0 ;
+
+Dog.prototype.calculatePrice = function () {
+
+    switch (this.age) {
+        case 'Adult':
+            if (this.breed === 'Bull Dog') {
+                this.price = 50000;
+            } else if (this.breed === 'Belgian Malinois') {
+                this.price = 60000;
+            } else if (this.breed === 'Rotweiler') {
+                this.price = 70000;
+            } else if (this.breed === 'German Shepherd') {
+                this.price = 80000;
+            } else if (this.breed === 'The Dalmatian') {
+                this.price = 90000;
+            } else if (this.breed === 'The Boxer') {
+                this.price = 100000;
+            } else if (this.breed === 'The Poodle') {
+                this.price = 110000;
+            } else if (this.breed === 'The Golden Retriever') {
+                this.price = 120000;
+            } else if (this.breed === 'Klein') {
+                this.price = 130000;
+            } else if (this.breed === 'Japanese Splitz') {
+                this.price = 140000;
+            } else if (this.breed === 'pugdog') {
+                this.price = 150000;
+            } else if (this.breed === 'Labrador Retriever') {
+                this.price = 160000;
+            }
+            break;
+
+        case 'Puppy':
+        
+            if (this.breed === 'Bull Dog') {
+                this.price = 25000;
+            } else if (this.breed === 'Belgian Malinois') {
+                this.price = 30000;
+            } else if (this.breed === 'Rotweiler') {
+                this.price = 35000;
+            } else if (this.breed === 'German Shepherd') {
+                this.price = 40000;
+            } else if (this.breed === 'The Dalmatian') {
+                this.price = 25000;
+            } else if (this.breed === 'The Boxer') {
+                this.price = 30000;
+            } else if (this.breed === 'The Poodle') {
+                this.price = 35000;
+            } else if (this.breed === 'The Golden Retriever') {
+                this.price = 40000;
+            } else if (this.breed === 'The Golden Retriever') {
+                this.price = 45000;
+            } else if (this.breed === 'Klein') {
+                this.price = 50000;
+            } else if (this.breed === 'Japanese Splitz') {
+                this.price = 55000;
+            } else if (this.breed === 'pugdog') {
+                this.price = 60000;
+            } else if (this.breed === 'Labrador Retriever') {
+                this.price = 65000;
+            }
+            break;
+
+
     }
-    return this.price ;
+
+    return this.price;
+
+
 }
-
-
 $(document).ready(function(){
     $("#checky").click(function(){
         $("#myForm").toggle("5000")
