608d4d7dba6973b8f0195583309c27e20ab72cf2
njerittah <vivianbianca92@gmail.com>
2020-03-06 09:50:26 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ Dog.prototype.calculatePrice = function () {
             break;
 
         case 'Puppy':
-        
+
             if (this.breed === 'Bull Dog') {
                 this.price = 25000;
             } else if (this.breed === 'Belgian Malinois') {
@@ -90,7 +90,7 @@ Dog.prototype.calculateDelivery = function () {
     return this.deliveryFee;
 }
 
-    return this.price;
+
 
 
 //Some javascript effects
@@ -131,46 +131,150 @@ $(document).ready(function () {
             var dogAge = $('#age-1 option:selected').val();
             var delivered = $('#delivery-1 option:selected').val();
             var quantity = parseInt($('#quantity-1').val());
+            var orderID = "orderID: PzA" + Math.floor(Math.random() * 7889887);
 
             var newDog = new Dog(dogBreed, dogAge, delivered);
+
+            $("ul#orders-1").append("<li><span class='myOrder'>" + orderID + " (view order)" +  "</span></li>");
+            
             dogPrice = newDog.calculatePrice();
             dogDeliveryFee = newDog.calculateDelivery();
 
+            if (newDog.mode === 'No Delivery') {
+                
+                alert('Thank you for shopping with us.Please pick your order at our stores')
+                
+                $('.myOrder').last().click(function () {
+                   
+                    $('.output-1').show();
+                    $('.dog-breed').text(newDog.breed);
+                    $('.dog-age').text(newDog.age);
+                    $('.dog-quantity').text(quantity);
+                    $('.dog-price').text(newDog.price);
+                    $('.dog-delivery').text(newDog.deliveryFee);
+                    $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
+                   
+
+                })
+
+            } else {
+
+                $("#address-1").append('<div class="new-address style:"background-color:blue; color:black ;">' +
+                    '<div class="form-group">' +
+                    '<div class="form-group">' +
+                    '<label for="phone-no">Phone number</label>' +
+                    '<input type="number" placeholder="Enter phone number.." class="form-control new-city">' +
+                    '</div>' +
+                    '<div class="form-group">' +
+                    '<label for="street-name">Street name</label>' +
+                    '<input type="text"  placeholder="Enter street name.." class="form-control new-county">' +
+                    '</div>' +
+                    '<div class="form-group">' +
+                    '<label for="county">county/city</label>' +
+                    '<input type="text"  placeholder="Enter county/city name..." class="form-control new-county">' +
+                    '</div>' +
+                    '</div>' +
+                    '<button type="submit" class="btn btn-info btn-block" id="confirm-1">Confirm</button><br><br></br>'
+
+                );
+                $('#confirm-1').click(function (event) {
+                    alert('Thank you for shopping with us.Please pick your order at our stores')
+                    
+                    $('.myOrder').last().click(function (event) {
+                        $('.output-2').show();
+                        $('.dog-breed').text(newDog.breed);
+                        $('.dog-age').text(newDog.age);
+                        $('.dog-quantity').text(quantity);
+                        $('.dog-price').text(newDog.price);
+                        $('.dog-delivery').text(newDog.deliveryFee);
+                        $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
+                        
+                        event.preventDefault();
+                    })
+                    event.preventDefault();
+                })
+
+
+            }
 
 
 
-            $('.output').show();
-            $('.dog-breed').text(newDog.breed);
-            $('.dog-age').text(newDog.age);
-            $('.dog-quantity').text(quantity);
-            $('.dog-price').text(newDog.price);
-            $('.dog-delivery').text(newDog.deliveryFee);
-            $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
 
 
 
         } else if (this.id === 'service') {                        //To purchase a service dog
             $('#guard').prop('disabled', true);
             $('#sniff').prop('disabled', true);
+            
 
 
             var dogBreed = $('#breed-2 option:selected').val();
             var dogAge = $('#age-2 option:selected').val();
             var delivered = $('#delivery-2 option:selected').val();
             var quantity = parseInt($('#quantity-2').val());
+            var orderID = "orderID: PzA" + Math.floor(Math.random() * 7889887);
 
             var newDog = new Dog(dogBreed, dogAge, delivered);
+            $("ul#orders-2").append("<li><span class='myOrder'>" + orderID + " (view order)" +  "</span></li>");
             dogPrice = newDog.calculatePrice();
             dogDeliveryFee = newDog.calculateDelivery();
 
+            if (newDog.mode === 'No Delivery') {
+                alert('Thank you for shopping with us.Please pick your order at our stores')
+                
+                $('.myOrder').last().click(function () {
+                    $('.output-2').show();
+                    $('.dog-breed').text(newDog.breed);
+                    $('.dog-age').text(newDog.age);
+                    $('.dog-quantity').text(quantity);
+                    $('.dog-price').text(newDog.price);
+                    $('.dog-delivery').text(newDog.deliveryFee);
+                    $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
+                   
 
-            $('.output').show();
-            $('.dog-breed').text(newDog.breed);
-            $('.dog-age').text(newDog.age);
-            $('.dog-quantity').text(quantity);
-            $('.dog-price').text(newDog.price);
-            $('.dog-delivery').text(newDog.deliveryFee);
-            $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
+                })
+
+            } else {
+
+                $("#address-2").append('<div class="new-address style:"background-color:blue; color:black ;">' +
+                    '<div class="form-group">' +
+                    '<div class="form-group">' +
+                    '<label for="phone-no">Phone number</label>' +
+                    '<input type="number" placeholder="Enter phone number.." class="form-control new-city">' +
+                    '</div>' +
+                    '<div class="form-group">' +
+                    '<label for="street-name">Street name</label>' +
+                    '<input type="text"  placeholder="Enter street name.." class="form-control new-county">' +
+                    '</div>' +
+                    '<div class="form-group">' +
+                    '<label for="county">county/city</label>' +
+                    '<input type="text"  placeholder="Enter county/city name..." class="form-control new-county">' +
+                    '</div>' +
+                    '</div>' +
+                    '<button type="submit" class="btn btn-info btn-block" id="confirm-2">Confirm</button><br><br></br>'
+
+                );
+                $('#confirm-2').click(function (event) {
+                    alert('Thank you for shopping with us.Please pick your order at our stores')
+                    
+                    $('.myOrder').last().click(function (event) {
+                        $('.output-2').show();
+                        $('.dog-breed').text(newDog.breed);
+                        $('.dog-age').text(newDog.age);
+                        $('.dog-quantity').text(quantity);
+                        $('.dog-price').text(newDog.price);
+                        $('.dog-delivery').text(newDog.deliveryFee);
+                        $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
+                        
+                        event.preventDefault();
+                    })
+                    event.preventDefault();
+                })
+
+            }
+
+
+
 
 
 
@@ -184,34 +288,34 @@ $(document).ready(function () {
             var dogAge = $('#age-3 option:selected').val();
             var delivered = $('#delivery-3 option:selected').val();
             var quantity = parseInt($('#quantity-3').val());
+            var orderID = "orderID: PzA" + Math.floor(Math.random() * 7889887);
 
             var newDog = new Dog(dogBreed, dogAge, delivered);
             dogPrice = newDog.calculatePrice();
+            $("ul#orders-3").append("<li><span class='myOrder'>" + orderID + " (view order)" +  "</span></li>");
             dogDeliveryFee = newDog.calculateDelivery();
 
             if (newDog.mode === 'No Delivery') {
-                $('.output').show();
-                $('.dog-breed').text(newDog.breed);
-                $('.dog-age').text(newDog.age);
-                $('.dog-quantity').text(quantity);
-                $('.dog-price').text(newDog.price);
-                $('.dog-delivery').text(newDog.deliveryFee);
-                $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
+                alert('Thank you for shopping with us.Please pick your order at our stores')
+                $('.myOrder').last().click(function(event) {
+                    $('.output-3').show();
+                    $('.dog-breed').text(newDog.breed);
+                    $('.dog-age').text(newDog.age);
+                    $('.dog-quantity').text(quantity);
+                    $('.dog-price').text(newDog.price);
+                    $('.dog-delivery').text(newDog.deliveryFee);
+                    $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
+                    
+                    event.preventDefault();
+
+                })
+              
 
             } else {
-                $('.output').show();
-                $('.dog-breed').text(newDog.breed);
-                $('.dog-age').text(newDog.age);
-                $('.dog-quantity').text(quantity);
-                $('.dog-price').text(newDog.price);
-                $('.dog-delivery').text(newDog.deliveryFee);
-                $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
 
-                $("#address-3").append('<div class="new-address bg-success">' +
+
+                $("#address-3").append('<div class="new-address style:"background-color:blue; color:black ;">' +
                     '<div class="form-group">' +
-                    '<label for="new-street">Name</label>' +
-                    '<input type="text" placeholder="Enter Name.." class="form-control new-street">' +
-                    '</div>' +
                     '<div class="form-group">' +
                     '<label for="phone-no">Phone number</label>' +
                     '<input type="number" placeholder="Enter phone number.." class="form-control new-city">' +
@@ -228,14 +332,26 @@ $(document).ready(function () {
                     '<button type="submit" class="btn btn-info btn-block" id="confirm-3">Confirm</button><br><br></br>'
 
                 );
+                $('#confirm-3').click(function (event) {
+                    alert('Thank you for shopping with us.Please pick your order at our stores')
+                    
+                    $('.myOrder').last().click(function(event) {
+                        $('.output-3').show();
+                        $('.dog-breed').text(newDog.breed);
+                        $('.dog-age').text(newDog.age);
+                        $('.dog-quantity').text(quantity);
+                        $('.dog-price').text(newDog.price);
+                        $('.dog-delivery').text(newDog.deliveryFee);
+                        $('.dog-total').text((quantity * newDog.price) + newDog.deliveryFee);
+                        
+                        event.preventDefault();
+                    })
+                    event.preventDefault();
+                })
 
 
 
             }
-
-
-
-
 
 
         }
@@ -248,14 +364,14 @@ $(document).ready(function () {
 })
 
 
-//navigation bar
+
 
 $(window).on("scroll", function () {
     if ($(window).scrollTop()) {
-        $('nav').addClass('black');
+     $('nav').addClass('black');
+     }
+     else {
+         $('nav').removeClass('black');
     }
- 
-    else {
-        $('nav').removeClass('black');
-    }
-})
+ })
+
